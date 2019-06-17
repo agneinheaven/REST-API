2532cfa0787ed523bce4dcee61332ebab220bada
agneinheaven <31819409+agneinheaven@users.noreply.github.com>
2019-06-17 12:56:55 +0300

    Add files via upload

---
+++ spring-boot-quick-start-master/src/main/java/com/codeatomic/springbootquickstart/hello/HelloControllerTest.java
@@ -0,0 +1,20 @@
+/*package com.codeatomic.springbootquickstart.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class HelloControllerTest {
+
+    @Autowired
+    private HelloController controller =  new HelloController();
+
+    @Test
+    public void contexLoads() throws Exception {
+    	//System.out.println(controller.helloWorld());
+       assertEquals("Hello World!",controller.helloWorld());
+    }
+}*/
